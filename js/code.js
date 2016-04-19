f3b400f908d4ef84a9beac80466c3b2a6508d13d
kadenhendron <kadenhendron@gmail.com>
2016-04-18 20:27:31 -0600

    Filter card in sidebar

--- js/code.js
+++ js/code.js
@@ -1,29 +1,30 @@
 $( document ).ready(function() {
-	var offset = $(".x-axis-container").offset().top;
+	var $el = $('.x-axis-container'); 
+	var offset = $el.offset().top;
 	
 	$(window).scroll(function(e){ 
-		var $el = $('.x-axis-container'); 
 		var isPositionFixed = ($el.css('position') == 'fixed');
 		var left_offset = $("#chart-container").offset().left;
 		var right_offset = ($(window).width() - ($("#chart-container").offset().left + $("#chart-container").outerWidth()));
 
 		if ($(this).scrollTop() > offset && !isPositionFixed){ 
 			$('.x-axis-container').css({'position': 'fixed', 'top': '0px', 'left': left_offset });
-			$('#legend').css({'position': 'fixed', 'top': '18px', 'right': right_offset });
+			$('#side-content').css({'position': 'fixed', 'top': '18px', 'right': right_offset });
 		}
 		if ($(this).scrollTop() < offset) {
 			$('.x-axis-container').css({'position': 'absolute', 'top': '-8px', 'left': 0});
-			$('#legend').css({'position': 'absolute', 'top': '6px', 'right': 0});
+			$('#side-content').css({'position': 'absolute', 'top': '6px', 'right': 0});
 		} 
 	});
 	
-	$(window).resize(function(){ 
+	$(window).resize(function(){
+		var isPositionFixed = ($el.css('position') == 'fixed');
 		var left_offset = $("#chart-container").offset().left;
 		var right_offset = ($(window).width() - ($("#chart-container").offset().left + $("#chart-container").outerWidth()));
 		
 		if ($(this).scrollTop() > offset && !isPositionFixed){ 
 			$('.x-axis-container').css({'position': 'fixed', 'top': '0px', 'left': left_offset });
-			$('#legend').css({'position': 'fixed', 'top': '18px', 'right': right_offset });
+			$('#side-content').css({'position': 'fixed', 'top': '18px', 'right': right_offset });
 		}
 	});
 });
