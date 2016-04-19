2c255e1619b7bc141e83d7a2866391f57ffa280f
kadenhendron <kadenhendron@gmail.com>
2016-04-18 22:24:46 -0600

    Position sidebar on load

--- js/code.js
+++ js/code.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
 	var $el = $('.x-axis-container'); 
-	var offset = $el.offset().top;
+	var offset = $el.offset().top;\
+	
+	var isPositionFixed = ($el.css('position') == 'fixed');
+		var left_offset = $("#chart-container").offset().left;
+		var right_offset = ($(window).width() - ($("#chart-container").offset().left + $("#chart-container").outerWidth()));
+		
+		if ($(this).scrollTop() > offset && !isPositionFixed){ 
+			$('.x-axis-container').css({'position': 'fixed', 'top': '0px', 'left': left_offset });
+			$('#side-content').css({'position': 'fixed', 'top': '18px', 'right': right_offset });
+		}
 	
 	$(window).scroll(function(e){ 
 		var isPositionFixed = ($el.css('position') == 'fixed');
